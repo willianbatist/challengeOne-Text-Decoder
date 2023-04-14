a0baa83fa66094f60ec9aaa0a79f726e9e132164
Willian <willian.alves.b15@gmail.com>
2023-04-14 15:45:42 -0300

    feat: finish logic js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const btnCriptografar = document.getElementById("btn-criptografar");
+const buttonEncrypt = document.getElementById("btn-criptografar");
+const buttonDecrypt = document.getElementById("btn-descriptografar");
 const textNone = document.getElementById("text-view");
 const textDecoder = document.getElementById("view-decoder");
 const p = document.getElementById("text");
 const textarea = document.getElementById("textarea");
+const buttonCopy = document.getElementById("copy");
 const A = /A/ig;
 const E = /E/ig;
 const I = /I/ig;
@@ -39,11 +41,34 @@ function handleTextDecoder(text) {
   return newText;
 }
 
+function handleTextEncoder(text) {
+  text = text.replace(/ai/g, "a");
+  text = text.replace(/enter/g, "e");
+  text = text.replace(/imes/g, "i");
+  text = text.replace(/ober/g, "o");
+  text = text.replace(/ufat/g, "u");
+  return text;
+}
+
 function clickDecoder() {
   textNone.style.display="none";
   textDecoder.style.display="block";
   p.innerHTML=handleTextDecoder(textarea.value);
+  textarea.value="";
+}
+
+function clickEncoder() {
+  p.innerHTML=handleTextEncoder(textarea.value);
+  textarea.value="";
+}
+
+function handleCopy() {
+  navigator.clipboard.writeText(p.innerText);
 }
 
 
-btnCriptografar.addEventListener("click", clickDecoder);
+
+
+buttonEncrypt.addEventListener("click", clickDecoder);
+buttonCopy.addEventListener("click", handleCopy);
+buttonDecrypt.addEventListener("click", clickEncoder);
